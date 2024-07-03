224742a8ecf24e98c7c766f47bdc3dc2d2dcb057
Aman Waghmare <amanwaghmare311@gmail.com>
2024-07-03 15:40:04 +0530

    converter is working fine

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
-import './App.css'
-import Inputbox from './components/currencyConverter/Inputbox'
-
+import { useState } from "react";
+import "./App.css";
+import Inputbox from "./components/currencyConverter/Inputbox";
+import useCurrencyInfo from "./components/currencyConverter/cutomehooks/useCurrencyInfo";
 function App() {
+  const [selectedValue, setSelectedValue] = useState(0);
+  const [responseValue, setResponseValue] = useState(0);
+  // eslint-disable-next-line no-unused-vars
+  const [valueDisabled, setValueDisabled] = useState(false);
+  const [fromCurrency, setFromCurrency] = useState('usd');
+  const [toCurrency, setToCurrency] = useState('inr');
 
+  const currencyInfo = useCurrencyInfo(fromCurrency);
+  const currencyOptions = Object.keys(currencyInfo);
+
+  const swap = () => {
+    setSelectedValue(responseValue);
+    setResponseValue(selectedValue);
+    setFromCurrency(toCurrency);
+    setToCurrency(fromCurrency);
+  };
+
+  const convert = () => {
+    setResponseValue(selectedValue * currencyInfo[toCurrency]);
+  };
   return (
-    <div className='mostouterbox'>
-     <Inputbox/>
-     <button className="swapbutton">Spap</button>
-     <Inputbox/>
+    <div className="mostouterbox">
+      <Inputbox
+        label="From"
+        selectedValue={selectedValue}
+        valueDisabled={valueDisabled}
+        currencyOptions={currencyOptions}
+        CurrencyType={fromCurrency}
+        onValueChange={(e) => setSelectedValue(e.target.value)}
+        onCurrencyChange={(e) => setFromCurrency(e.target.value)}
+      />
+      <button className="button" onClick={swap}>
+        Spap
+      </button>
+      <Inputbox
+        label="To"
+        selectedValue={responseValue}
+        valueDisabled={true}
+        currencyOptions={currencyOptions}
+        CurrencyType={toCurrency}
+        onValueChange={(e) => responseValue(e.target.value)}
+        onCurrencyChange={(e) => setToCurrency(e.target.value)}
+      />
+      <button className='button' onClick={convert}>Convert {fromCurrency.toUpperCase()} to {toCurrency.toUpperCase()}</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/currencyConverter/Inputbox.jsx
+++ src/components/currencyConverter/Inputbox.jsx
@@ -1,27 +1,44 @@
-import "./inputbox.css"
+/* eslint-disable react/prop-types */
+import "./inputbox.css";
 function Inputbox({
-label="from",
-
+  label,
+  selectedValue,
+  valueDisabled,
+  currencyOptions,
+  CurrencyType,
+  onValueChange,
+  onCurrencyChange
 }) {
-
   return (
     <div className="outerbox">
-        <div className="box1">
-            <div className="leftbox">
-                <label htmlFor="">{label}</label>
-                <input type="number" />
-                
-            </div>
-            <div className="rightbox">
-                <label htmlFor="">Currency Options</label>
-                <select>
-                    <option value=""></option>
-                </select>
-            </div>
+      <div className="box1">
+        <div className="leftbox">
+          <label htmlFor="">{label}</label>
+          <input
+            className="line2 line1"
+            type="number"
+            defaultValue="0"
+            disabled={valueDisabled}
+            value={selectedValue}
+            onChange={onValueChange}
+          />
         </div>
-       
+        <div className="rightbox">
+          <label htmlFor="">Currency Options</label>
+          <select
+            className="line2"
+            defaultValue={CurrencyType}
+            onChange={onCurrencyChange}
+            value={CurrencyType}
+          >
+            {currencyOptions.map((Currency) => (
+              <option key={Currency}>{Currency}</option>
+            ))}
+          </select>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Inputbox
+export default Inputbox;
